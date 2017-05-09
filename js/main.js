981d35856cbb1b52ed4a7ba2f34d2a266425df69
QuentinPingouin <haentjensquentin@gmail.com>
2017-05-09 08:56:26 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,80 @@
+/*//smooth scroll
+$(document).ready(function () {
+$(window).scroll(function () {
+  var topWindow = $(window).scrollTop();
+  var topWindow = topWindow * 1.5;
+  var windowHeight = $(window).height();
+  var position = topWindow / windowHeight;
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(10) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+});
+
+
+
+//Actif menu
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#menu-center .link__menu').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.link__menu').removeClass("active__menu");
+            currLink.addClass("active__menu");
+        }
+        else{
+            currLink.removeClass("active__menu");
+        }
+    });
+}*/
+
+	$(document).ready(function () {
+		$(document).on("scroll", onScroll);
+ 
+		$('a[href^="#"]').on('click', function (e) {
+			e.preventDefault();
+			$(document).off("scroll");
+ 
+			$('a').each(function () {
+				$(this).removeClass('active');
+			})
+			$(this).addClass('active');
+ 
+			var target = this.hash;
+			$target = $(target);
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top+2
+			}, 500, 'swing', function () {
+				window.location.hash = target;
+				$(document).on("scroll", onScroll);
+			});
+		});
+	});
+ 
+	function onScroll(event){
+		var scrollPosition = $(document).scrollTop();
+		$('#menu-center a').each(function () {
+			var currentLink = $(this);
+			var refElement = $(currentLink.attr("href"));
+			if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+				$('nav ul li a').removeClass("active__menu");
+				currentLink.addClass("active__menu");
+			}
+			else{
+				currentLink.removeClass("active__menu");
+			}
+		});
+	}
